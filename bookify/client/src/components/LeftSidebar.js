147518a13ede751cc8fee3dc64b97f91d2ca33d0
Phillip Paul <phillippaul@gmx.de>
2020-05-15 14:21:56 +0200

    Updated: component structuring

---
+++ bookify/client/src/components/LeftSidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class LeftSidebar extends Component {
+  render() {
+    return (
+      <div className="left-sidebar">
+        <div className="left-section">User data</div>
+        <div className="left-section">List data</div>
+      </div>
+    );
+  }
+}
+
+export default LeftSidebar;
